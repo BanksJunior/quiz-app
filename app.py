7e2de3230d2c7b08cee95dfd7997510622f0351c
BanksJunior <banksjunior06@gmail.com>
2026-04-23 09:58:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from questions import questions
-import random
 
 app = Flask(__name__)
 app.secret_key = "quiz_secret_key"
@@ -54,5 +53,10 @@ def quiz():
 def result():
     return render_template("result.html", score=session.get("score", 0))
 
+
+# ✅ RAILWAY FIX (IMPORTANT)
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
